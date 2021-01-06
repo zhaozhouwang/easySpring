3b34e204a3b4b38fc0470c95b9199b45987befaa
zhaozhouwang <876641116@qq.com>
2021-01-06 11:31:04 +0800

    commit

--- src/main/java/com/lab/one/controller/UserController.java
+++ src/main/java/com/lab/one/controller/UserController.java
@@ -1,31 +1,18 @@
 package com.lab.one.controller;
 
-import com.lab.one.entity.LabUser;
+import com.baomidou.mybatisplus.toolkit.StringUtils;
+import com.lab.one.controller.base.BaseController;
 import com.lab.one.service.LabUserService;
+import com.lab.one.utils.Response;
 import com.lab.one.vo.LoginParam;
-import org.springframework.beans.BeanUtils;
+import com.lab.one.vo.UserResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.baomidou.mybatisplus.plugins.Page;
-import com.baomidou.mybatisplus.toolkit.StringUtils;
-import com.lab.one.controller.base.BaseController;
-import com.lab.one.enums.ResponseErrorEnum;
-import com.lab.one.utils.Response;
-import com.lab.one.vo.UserResult;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
 
 /**
  * <p>
@@ -46,7 +33,7 @@ public class UserController extends BaseController {
 
 
     @ApiOperation(value = "登录", notes = "登录", response = UserResult.class)
-    @PostMapping("/login")
+    @PostMapping("/public/login")
     public Response login(@RequestBody LoginParam param) {
         String token = userService.userLogin(param);
         return StringUtils.isEmpty(token) ? Response.fail() : Response.success(token);
